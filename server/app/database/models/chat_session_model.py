e23d350ca6b8c3a5b0361fde537e1855d28b4369
Suleman <suleman.badar.butt@gmail.com>
2026-07-15 20:06:41 +0500

    Resolved some naming conflicts

---
+++ server/app/database/models/chat_session_model.py
@@ -0,0 +1,69 @@
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, func, Uuid, DateTime
+
+from app.database.base_model import Base
+from app.database.models.message_model import Message
+
+import uuid
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.database.models.user_model import User
+    from app.database.models.video_model import Video
+
+
+class ChatSession(Base):
+    __tablename__ = "chat_sessions"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid,
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        Uuid,
+        ForeignKey(
+            "users.id",
+            ondelete="CASCADE"
+        ),
+        nullable=False,
+    )
+        
+    
+    video_id: Mapped[uuid.UUID] = mapped_column(
+        Uuid,
+        ForeignKey("videos.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
+
+    title: Mapped[str] = mapped_column(
+        String(200),
+        default="New Chat"
+    )
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    
+    messages : Mapped[list["Message"]] = relationship(
+        back_populates="session",
+        cascade="all, delete-orphan"
+    )
+    
+    user : Mapped["User"] = relationship(
+        back_populates="chat_sessions"
+    )
+    
+    video: Mapped["Video"] = relationship(
+        back_populates="chat_sessions"
+    )
---
+++ server/app/database/models/user_model.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, func, Uuid, DateTime, Text
+
+from app.database.base_model import Base
+
+import uuid
+from datetime import datetime
+
+from app.database.models.chat_session_model import ChatSession
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid,
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now()
+    )
+    
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+    
+    chat_sessions = Mapped[list["ChatSession"]] = relationship( 
+        back_populates="user",
+        cascade="all, delete-orphan"
+    )
+    
