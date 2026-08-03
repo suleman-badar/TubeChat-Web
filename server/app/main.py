c75b15cf69ec49680d21d1b4e5ac4c4d3cab8a71
Suleman Badar <suleman.badar.butt@gmail.com>
2026-08-03 18:08:57 +0500

    Uncomment allowed origin for CORS middleware

--- server/app/main.py
+++ server/app/main.py
@@ -34,7 +34,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        # "https://tube-chat-web.vercel.app",
+        "https://tube-chat-web.vercel.app",
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
